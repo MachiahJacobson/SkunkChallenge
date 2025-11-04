b4cdb56e47e0355a849d24dc92f04371a68f6760
Mac <macjacobson99@gmail.com>
2025-11-04 14:11:04 -0700

    Initial Commit

---
+++ SkunkChallenge/SkunkChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkunkChallengeApp.swift
+//  SkunkChallenge
+//
+//  Created by Jacobson, Machiah - Student on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SkunkChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SkunkChallengeTests/SkunkChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SkunkChallengeTests.swift
+//  SkunkChallengeTests
+//
+//  Created by Jacobson, Machiah - Student on 11/4/25.
+//
+
+import Testing
+@testable import SkunkChallenge
+
+struct SkunkChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
